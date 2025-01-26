c4872732860e2cda16a79a829c80f31b4888a9a5
sarayu10062005 <srijasarayu@gmail.com>
2025-01-26 03:00:14 -0800

    Add files via upload

---
+++ CondOpt.java
@@ -0,0 +1,7 @@
+public class CondOpt {
+    public static void main(String[] args) {
+        int n = 12;
+        String result = (n % 2 == 0) ? "Even!" : "Odd!";
+        System.out.println(result);
+    }
+}
